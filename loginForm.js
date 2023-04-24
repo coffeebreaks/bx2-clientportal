b376b0f8d4a1640e15357cc491c0df796495ecba
Coffeebreaks <stefan@grafikcentralen.se>
2023-04-25 00:10:06 +0200

    asd

---
+++ loginForm.js
@@ -0,0 +1,18 @@
+export class LoginForm extends HTMLElement {
+    constructor() {
+      super();
+  
+      fetch("loginForm.html")
+        .then((response) => response.text())
+        .then((html) => {
+          const template = document.createElement("template");
+          template.innerHTML = html;
+          this.appendChild(template.content.cloneNode(true));
+        })
+        .catch((error) => {
+          console.error("Error loading HTML file:", error);
+        });
+    }
+  }
+  
+  customElements.define("login-form", LoginForm);
---
+++ userManager.js
@@ -0,0 +1,18 @@
+export class UserManager extends HTMLElement {
+    constructor() {
+      super();
+  
+      fetch("userManager.html")
+        .then((response) => response.text())
+        .then((html) => {
+          const template = document.createElement("template");
+          template.innerHTML = html;
+          this.appendChild(template.content.cloneNode(true));
+        })
+        .catch((error) => {
+          console.error("Error loading HTML file:", error);
+        });
+    }
+  }
+  
+  customElements.define("user-manager", UserManager);
